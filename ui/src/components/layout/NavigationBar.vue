bcd02810b2b8fa31a7eb624c6ebc5491bd3a60d5
Livi005 <ibanezmustelier@gmail.com>
2025-09-16 21:10:38 -0400

    add navigation bar

---
+++ ui/src/components/layout/NavigationBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navigation-bar">
+    <div class="navigation-brand">
+      <i class="fab fa-spotify navigation-brand-icon"></i>
+      <span>Spotify Clone</span>
+    </div>
+    <div class="navigation-actions">
+      <button class="btn btn-outline-secondary me-2">Iniciar Sesión</button>
+      <button class="btn btn-primary">Cerrar Sesión</button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavigationBar'
+}
+</script>
+
+<style scoped src="@/assets/styles/components.css"></style>
