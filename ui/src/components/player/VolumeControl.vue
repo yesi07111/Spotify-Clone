2484cae344ba6c4d86ae2c04275070cfe176f95b
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-09-19 10:15:54 -0400

    add basic volumen control

---
+++ ui/src/components/player/VolumeControl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="volume-control-container">
+    <i class="fas fa-volume-up volume-icon"></i>
+    <input
+      type="range"
+      class="volume-slider"
+      min="0"
+      max="100"
+      :value="volumeLevel"
+      @input="updateVolumeLevel"
+      aria-label="Control de volumen"
+    >
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'VolumeControl',
+  computed: {
+    ...mapState('player', ['volumeLevel'])
+  },
+  methods: {
+    ...mapActions('player', ['updateVolumeLevel'])
+  }
+}
+</script>
+
+<style scoped src="@/assets/styles/components.css"></style>
