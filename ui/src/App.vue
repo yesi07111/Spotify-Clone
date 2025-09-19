24b24365e38df634c81679072ba7b24ae73dfca9
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-09-19 10:20:07 -0400

    add basic main entry point

---
+++ ui/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <NavigationBar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavigationBar from '@/components/layout/NavigationBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<style>
+@import '@/assets/styles/main.css';
+</style>
