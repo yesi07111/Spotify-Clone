4d9b8fb8a43fb3f0dace756aa140e6f857f83d2c
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-09-19 10:17:12 -0400

    update TrackItem for use random image

--- ui/src/components/tracks/TrackItem.vue
+++ ui/src/components/tracks/TrackItem.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="track-item" @click="selectTrack">
-    <img :src="trackImageUrl" :alt="trackTitle" class="track-item-image">
+    <img 
+      :src="currentImageUrl" 
+      :alt="trackTitle" 
+      class="track-item-image"
+      @error="handleImageError"
+    >
     <div class="track-item-info">
       <div class="track-item-title">{{ trackTitle }}</div>
       <div class="track-item-details">{{ trackArtist }} • {{ trackAlbum }}</div>
@@ -10,7 +15,7 @@
 </template>
 
 <script>
-import { getTrackImageUrl } from '@/utils/imageHelper'
+import { getTrackImageUrl, getRandomDefaultImage } from '@/utils/imageHelper'
 
 export default {
   name: 'TrackItem',
@@ -20,10 +25,13 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      currentImageUrl: '',
+      imageError: false
+    }
+  },
   computed: {
-    trackImageUrl() {
-      return getTrackImageUrl(this.track.title, this.track.id)
-    },
     trackTitle() {
       return this.track.title || 'Título desconocido'
     },
@@ -37,12 +45,31 @@ export default {
       return this.track.duration || '0:00'
     }
   },
+  mounted() {
+    this.loadImage()
+  },
   methods: {
     selectTrack() {
       this.$emit('track-selected', this.track)
+    },
+    loadImage() {
+      this.currentImageUrl = getTrackImageUrl(this.track.title, this.track.id)
+    },
+    handleImageError() {
+      if (!this.imageError) {
+        this.imageError = true
+        this.currentImageUrl = getRandomDefaultImage(this.track.id)
+      }
+    }
+  },
+  watch: {
+    track: {
+      handler() {
+        this.imageError = false
+        this.loadImage()
+      },
+      deep: true
     }
   }
 }
 </script>
-
-<style scoped src="@/assets/styles/components.css"></style>
